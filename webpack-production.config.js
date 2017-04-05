57b5af226e4c6f43362459a9182bd8f19fd567fb
Sven Anders Robbestad <robbestad@gmail.com>
2017-04-05 19:45:04 +0200

    Set modules to false for webpack2 tree shaking

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -56,7 +56,7 @@ module.exports = {
         query: {
           presets: [
             'react',
-            'es2015'
+            ['es2015', {modules: false}]
           ],
           plugins: [
           ]
