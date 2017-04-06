22a06bc3e7e66e34a3ea21e09a5fddbccd04f4d8
Sven Anders Robbestad <robbestad@gmail.com>
2017-04-06 09:02:55 +0200

    Added prettier

--- source/index.js
+++ source/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import App from './app';
 const rootEl = document.getElementById('app');
-import '../assets/style.css'
-const renderApp = (Component = App) => render(
-  React.createElement(App),
-  rootEl
-);
+import '../assets/style.css';
+const renderApp = (Component = App) => render(React.createElement(App), rootEl);
 
 if (module.hot) {
   module.hot.accept();
